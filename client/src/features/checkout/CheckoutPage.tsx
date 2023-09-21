2a248a8464d2b5d4b2abefc1cb903ce3c2d765de
jamesrcraigie <jamesrcraigie1@gmail.com>
2023-09-21 17:21:24 +0100

    Section 6

---
+++ client/src/features/checkout/CheckoutPage.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function CheckoutPage() {
+    return (
+        <Typography variant='h2'>
+        Only logged in users will be able to use this!
+    </Typography>
+    )
+}
