8bbbac0ca46a6fcbc6b9f8980bca735d576513ff
jamesrcraigie <jamesrcraigie1@gmail.com>
2023-11-09 21:18:25 +0000

    Section 9

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -2,6 +2,7 @@ import axios, {AxiosError, AxiosResponse} from"axios";
 import { toast } from "react-toastify";
 import { router } from "../router/Routes";
 import { PaginatedRespose } from "../models/pagination";
+import { store } from "../store/configureStore";
 
 const sleep = () => new Promise(reslove => setTimeout(reslove, 500));
 
@@ -9,6 +10,12 @@ axios.defaults.baseURL = 'http://localhost:5000/api/';
 axios.defaults.withCredentials = true;
 
 const responseBody = (reponse: AxiosResponse) => reponse.data;
+
+axios.interceptors.request.use(config => {
+    const token = store.getState().account.user?.token;
+    if (token) config.headers.Authorization =`Bearer ${token}`;
+    return config;
+})
 
 axios.interceptors.response.use(async response => {
     await sleep();
@@ -77,10 +84,17 @@ const Basket = {
     removeItem: (productId: number, quantity = 1) => requests.delete(`basket?productId=${productId}&quantity=${quantity}`)
 }
 
+const Account = {
+    login: (values: any) => requests.post('account/login', values),
+    register: (values: any) => requests.post('account/register', values),
+    currentUser: () => requests.get('account/currentUser')
+}
+
 const agent = {
     Catalog,
     TestErrors,
-    Basket
+    Basket,
+    Account
 }
 
 export default agent;
--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -3,6 +3,7 @@ import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typogr
 import { Link, NavLink } from "react-router-dom";
 import { useStoreContext } from "../context/StoreContext";
 import { useAppSelector } from "../store/configureStore";
+import SignedInMenu from "./SignedInMenu";
 
 const midLinks = [
     {title: 'catalog', path: '/catalog'},
@@ -35,6 +36,7 @@ interface Props {
 
 export default function Header({darkMode, handleThemeChange}: Props) {
     const {basket} = useAppSelector(state => state.basket);
+    const {user} = useAppSelector(state => state.account);
     const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0);
 
     return (
@@ -72,8 +74,11 @@ export default function Header({darkMode, handleThemeChange}: Props) {
                         <Badge badgeContent={itemCount} color='secondary'>
                             <ShoppingCart />
                         </Badge>
-                    </IconButton>      
-                    <List sx={{display:'flex'}}>
+                    </IconButton>
+                    {user ? (
+                        <SignedInMenu />
+                    ) : (
+                        <List sx={{display:'flex'}}>
                         {rightLinks.map(({title, path}) => (
                             <ListItem
                                 component={NavLink}
@@ -85,6 +90,7 @@ export default function Header({darkMode, handleThemeChange}: Props) {
                             </ListItem>
                         ) )}
                     </List>
+                    )}  
                 </Box>
 
             </Toolbar>
